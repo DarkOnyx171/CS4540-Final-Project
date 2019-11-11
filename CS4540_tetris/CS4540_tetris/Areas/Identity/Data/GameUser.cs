dcb417d151a1ec1f1093b8c7f0d5f226daa7d91c
Courtney Crum <u0829734@utah.edu>
2019-11-11 16:50:18 -0700

    Added identity

---
+++ CS4540_tetris/CS4540_tetris/Areas/Identity/Data/GameUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CS4540_tetris.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the GameUser class
+    public class GameUser : IdentityUser
+    {
+        override public string UserName { get; set; }
+    }
+}
---
+++ CS4540_tetris/CS4540_tetris/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CS4540_tetris.Areas.Identity.Data;
+using CS4540_tetris.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CS4540_tetris.Areas.Identity.IdentityHostingStartup))]
+namespace CS4540_tetris.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserContextConnection")));
+
+                services.AddDefaultIdentity<GameUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserContext>();
+            });
+        }
+    }
+}
