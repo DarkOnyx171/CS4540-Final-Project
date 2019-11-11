02237c95f3e49f517810cd04ecebc22bbf6dd1e6
HAYDEN HENRIE <u1057630@umail.utah.edu>
2019-11-11 13:56:19 -0700

    I added a project to the github hopefully i did it correctly

---
+++ CS4540_tetris/CS4540_tetris/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CS4540_tetris.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
