5b23f5f21dc3b120bb9218dcf262b5a6e89faaf5
HAYDEN HENRIE <u1057630@umail.utah.edu>
2019-11-11 15:24:33 -0700

    added basic pixi information and canvas

---
+++ CS4540_tetris/CS4540_tetris/wwwroot/js/tetris.js
@@ -0,0 +1,19 @@
+﻿//put a header here
+
+
+let type = "WebGL"
+
+if (!PIXI.utils.isWebGLSupported()) {
+    type = "canvas"
+}
+
+//Create a Pixi Application
+let app = new PIXI.Application({
+    width: 256,
+    height: 256,
+    view: document.getElementById("game")
+});
+
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.getElementById("game_area").appendChild(app.views);
