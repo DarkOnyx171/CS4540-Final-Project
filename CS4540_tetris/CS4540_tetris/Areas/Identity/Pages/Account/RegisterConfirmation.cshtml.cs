8fb58b67e25d248ba3e99a02825e24e59d2d28a4
Courtney Crum <u0829734@utah.edu>
2019-11-11 16:53:29 -0700

    Migrated Identity. Updated Database.

--- CS4540_tetris/CS4540_tetris/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ CS4540_tetris/CS4540_tetris/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.AspNetCore.Identity;
 
 namespace CS4540_tetris.Areas.Identity.Pages.Account
 {
