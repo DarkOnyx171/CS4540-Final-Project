680a0a18929087c302a002a06c8face14dfe3eed
HAYDEN HENRIE <u1057630@umail.utah.edu>
2019-11-14 13:46:34 -0700

    added a working board that can be added to and removed from

--- CS4540_tetris/CS4540_tetris/wwwroot/js/tetris.js
+++ CS4540_tetris/CS4540_tetris/wwwroot/js/tetris.js
@@ -2,30 +2,208 @@
 
 
 let type = "WebGL"
+let Application = PIXI.Application,
+    Sprite = PIXI.Sprite;
 
 if (!PIXI.utils.isWebGLSupported()) {
     type = "canvas"
 }
 
 //Create a Pixi Application
-let app = new PIXI.Application({
-    width: 256,
-    height: 256,
+let app = new Application({
+    width: 200,
+    height: 400,
     view: document.getElementById("game")
 });
 
-
-//Add the canvas that Pixi automatically created for you to the HTML document
-document.getElementById("game_area").appendChild(app.views);
-
-
+var graphics = new PIXI.Graphics();
+app.renderer.backgroundColor = 0x061639;
 
 const ROW = 20;
 const COL = COLUMN = 10;
+const SQ = squareSize = 20;
+const I = [
+    [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+    ],
+    [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+    ],
+    [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+    ]
+];
+
+const J = [
+    [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0]
+    ]
+];
+
+const L = [
+    [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
+    ],
+    [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const O = [
+    [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
+    ]
+];
+
+const S = [
+    [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+    ],
+    [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+    ],
+    [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const T = [
+    [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0]
+    ]
+];
+
+const Z = [
+    [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0]
+    ],
+    [
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0]
+    ],
+    [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1]
+    ],
+    [
+        [0, 1, 0],
+        [1, 1, 0],
+        [1, 0, 0]
+    ]
+];
+
+function drawSquare(x, y, color) {
+    graphics.beginFill(color);
+    graphics.drawRect(x*SQ, y*SQ, SQ, SQ); // drawRect(x, y, width, height)
+    graphics.endFill();
+}
 
 let board = [];
-let board2 = [];
+for (r = 0; r < ROW; r++) {
+    board[r] = [];
+    for (c = 0; c < COL; c++) {
+        board[r][c] = 0x000000;
+    }
+}
 
+//visual testing of the board creation
+board[0][0] = 0x00FF00;
+board[19][9] = 0xFF0000;
 
+// draw the board
+function drawBoard() {
+    for (r = 0; r < ROW; r++) {
+        for (c = 0; c < COL; c++) {
+            drawSquare(c, r, board[r][c]);
+        }
+    }
+    app.stage.addChild(graphics);
+}
 
+drawBoard();
 
+board[18][8] = 0xFF0000;
+board[19][9] = 0x000000;
+
+drawBoard();
