fa64781df56fd3ec3cb5ae31fa4e58a72631e422
jaecee.naylor <jaecee.scholarships@gmail.com>
2019-11-14 19:39:37 -0600

    Tried to add initializing stuff for scores... TODO enum configuation? Editing some looks, not sure if I like it, can't seem to get page names to spread out wihich is annoying

--- CS4540_tetris/CS4540_tetris/Data/DbInitializer.cs
+++ CS4540_tetris/CS4540_tetris/Data/DbInitializer.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +12,72 @@ namespace CS4540_tetris.Data
 {
     public class DbInitializer
     {
+        /// <summary>
+        /// this is to initialize the databases with data
+        /// </summary>
+        /// <param name="scorecontext"></param>
+        /// <param name="usercontext"></param>
+        /// <param name="userManager"></param>
         public static void Initialize(ScoreContext scorecontext, UserContext usercontext, UserManager<GameUser> userManager)
         {
             scorecontext.Database.Migrate();
             usercontext.Database.Migrate();
             SeedUsers(userManager);
+            SeedScores(scorecontext);
+        }
+
+        /// <summary>
+        /// this is to create scores if they do not exist
+        /// </summary>
+        /// <param name="scorecontext"></param>
+        /// <param name="usercontext"></param>
+        /// <param name="userManager"></param>
+        public static void SeedScores(ScoreContext scorecontext)
+        {
+            // Look for any scores
+            if (scorecontext.Scores.Any())
+            {
+                return;   // DB has been seeded
+            }
+            //Initialize these scores
+            var scores = new Score[]
+            {
+            new Score{
+                Value = 7000,
+                Nickname = "Boi",
+                UserName = "gameboi@tetrominoes.com",
+                //TODO how to initialize gamemode if enum?
+                //GameMode = 1;
+            },
+            new Score{
+                Value = 2000,
+                Nickname = "Gorl",
+                UserName = "gamegorl@tetrominoes.com",
+                //TODO how to initialize gamemode if enum?
+                //GameMode = 1;
+            },
+            new Score{
+                Value = 25,
+                Nickname = "Alien",
+                UserName = "gamealien@tetrominoes.com",
+                //TODO how to initialize gamemode if enum?
+                //GameMode = 1;
+            },
+            };
+            foreach (Score s in scores)
+            {
+                scorecontext.Scores.Add(s);
+            }
+
+            try
+            {
+                scorecontext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
         }
 
         /// <summary>
